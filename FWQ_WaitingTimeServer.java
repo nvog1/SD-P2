57dfff5bd9bc5c56b0ee4459786c35fe7a43b865
nicoslaptopW <nvog1@gcloud.ua.es>
2021-10-15 12:54:04 +0200

    implemented a part of the socket part of wts. considering how to manage the multiple connections from kafka and from socket, as ss.accept() blocks the flow of the program.

--- FWQ_WaitingTimeServer.java
+++ FWQ_WaitingTimeServer.java
@@ -1,8 +1,40 @@
-//parte de kafka
+import java.net.*;
+
+public class FWQ_WaitingTimeServer {
+
+	public static void main(String[] args) {
+
+		//parte de kafka
 
 
 
-//
+		//parte de socket server contra engine
+		
+		String puerto = "";
+		
+		try
+		{
+			if (args.length < 2) {
+				System.out.println("Indica el puerto de escucha y la ip y puerto del broker(puerto puerto:ip)");
+				System.exit(1);
+			}
+			puerto = args[0];
+			ServerSocket ss = new ServerSocket(Integer.parseInt(puerto));
+			System.out.println("Escucho el puerto " + puerto);
 
+			for(;;)
+			{
+				Socket cs = ss.accept();
+				System.out.println("Sirviendo al motor...");
 
-//parte de socket server contra engine
+				Thread t = new WTSHilo(cs);
+				t.start();
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error: " + e.toString());
+		}
+		
+	}
+}
