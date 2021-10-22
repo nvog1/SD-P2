694ded9adf4321c577834df9a1eb43d62079db73
JoseLopZar <71760093+JoseLopZar@users.noreply.github.com>
2021-10-22 11:48:54 +0200

    Añadido un poco de acceso a BD

--- FWQ_HiloServidorRegistro.java
+++ FWQ_HiloServidorRegistro.java
@@ -46,20 +46,20 @@ public class HiloServidorRegistro extends Thread {
 
     // Conexion con la Base de datos
     // URL de Conexion
-    String connectionURL = "jdbc:sqlserver://SERVER,database.windows.net:1433;" +
-        "database=BBDD;" + 
-        "user=USERNAME@SERVER;" + 
-        "password=PASSWORD;" +
-        "encrypt=true;" + 
-        "trusServerCertificate=false;" + 
-        "loginTimeout=30;";
-    // Conectar con la base de datos
+    /*String connectionURL = "jdbc:mysql://localhost:3306/NOMBRE_BD";
+    String user = "root";
+    String password = "1234";*/
+
+    // Realizar una consukta a la base de datos
     public boolean consultaSQL(){
-        try (Connection connection = DriverManager.getConnection(connectionURL)) {
+        String connectionURL = "jdbc:mysql://localhost:3306/NOMBRE_BD";
+        String user = "root";
+        String password = "1234";
+        try (Connection connection = DriverManager.getConnection(connectionURL, user, password)) {
             
         }
         catch (SQLExcepion e) {
-            System.out.println("Error: " + e.toString());
+            System.out.println("Error SQL: " + e.get.Message());
         }
     }
 
