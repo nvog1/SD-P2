e2f7febce3d125a39a6e853346ae001a11c332cf
nicoslaptopW <nvog1@gcloud.ua.es>
2021-10-29 10:18:18 +0200

    provisional commit. starting with kafka consumers, wts.

--- FWQ_WaitingTimeServer.java
+++ FWQ_WaitingTimeServer.java
@@ -9,7 +9,15 @@ public class FWQ_WaitingTimeServer {
 	public static void main(String[] args) {
 
 		//parte de kafka
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "broker1:9092,broker2:9092");
+		props.put("group.id", "CountryCounter");
+		props.put("key.deserializer",
+			"org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer",
+			"org.apache.kafka.common.serialization.StringDeserializer");
 
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 
 
 		//parte de socket server contra engine
