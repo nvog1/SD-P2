233d4eb8291b8fd119c3f5ae02695ed9df4f7630
nicoslaptopW <nvog1@gcloud.ua.es>
2021-11-05 12:34:42 +0100

    wtshilokafka basic implementation. should work for testing.

--- WTSHiloKafka.java
+++ WTSHiloKafka.java
@@ -1,13 +1,49 @@
+import java.net.*;
+import java.lang.Exception;
+import java.lang.reflect.Array;
+import java.net.Socket;
+import java.io.*;
 
+public class WTSHiloKafka extends Thread {
 
+	private String ipBroker;
+	private String puertoBroker;
+	
+	public WTSHiloKafka(String ipBroker, String puertoBroker){
+		this.ipBroker = ipBroker;
+		this.puertoBroker = puertoBroker;
+	}
 
-
-Properties props = new Properties();
-		props.put("bootstrap.servers", "broker1:9092,broker2:9092");
-		props.put("group.id", "CountryCounter");
-		props.put("key.deserializer",
-			"org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer",
-			"org.apache.kafka.common.serialization.StringDeserializer");
+	public void run(){
+		Properties props = new Properties();
+		props.put("bootstrap.servers", ipBroker + ":" + puertoBroker);
+		props.put("group.id", "WTS");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+		consumer.subscribe(Collections.singletonList("Sensores"));
+
+		Duration timeout = Duration.ofMillis(100);
+
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(timeout);
+
+			for (ConsumerRecord<String, String> record : records) {
+
+				//TEST
+				System.out.printf("topic = %s, partition = %d, offset = %d, " +
+								"customer = %s, country = %s\n",
+				record.topic(), record.partition(), record.offset(),
+						record.key(), record.value());
+				//TEST
+
+				//lógica del hilokafka	
+			}
+	}
+
+}
+
+
+
