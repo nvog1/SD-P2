a5ff42f05c0af67f1bc63824cef6659817aec689
nicoslaptopW <nvog1@gcloud.ua.es>
2021-11-19 12:38:29 +0100

    implemented part of wts bbdd.

--- WTSHiloKafka.java
+++ WTSHiloKafka.java
@@ -24,7 +24,7 @@ public class WTSHiloKafka extends Thread {
 		props.put("bootstrap.servers", ipBroker + ":" + puertoBroker);
 		props.put("group.id", "WTS");
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
 
 		try{
 
@@ -42,13 +42,27 @@ public class WTSHiloKafka extends Thread {
 				for (ConsumerRecord<String, String> record : records) {
 
 					//TEST
-					System.out.printf("topic = %s, partition = %d, offset = %d, " +
-									"customer = %s, country = %s\n",
-					record.topic(), record.partition(), record.offset(),
-							record.key(), record.value());
+					System.out.printf("Atracción: %s; personas = %d\n", record.key(), record.value());
 					//TEST
 
 					//lógica del hilokafka	
+					//actualizo el valor "personas" en la bbdd
+					try{
+						BufferedReader bufrd = new BufferedReader(new FileReader("C:\kafka\SD-P2\atracciones.txt"));
+						String line = bufrd.readLine();
+						while(line != null){
+							String[] items = line.split(";");
+							if(items[0] == record.key()){
+								//escribir la línea con personas = record.value() (crear una lista de string con las atracciones para luego escribirlas)
+							}
+
+							line = bufrd.readLine();
+						}
+					}
+					catch(Exception e){
+						System.out.println("Error: " + e.message);
+					}
+
 				}
 			}
 		}
